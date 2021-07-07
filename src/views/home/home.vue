373ca32f123b2e53e3ecacb3ac147259ba0f8de7
苟宏 <1878149080@qq.com>
2021-07-07 20:32:37 +0800

    完成推荐和本周推荐模块

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -2,6 +2,8 @@
     <div class="home">
       <home-nav></home-nav>
       <myswiper :swiperImage="swiperImage"></myswiper>
+      <myrecommend :recommendData="recommendData"></myrecommend>
+      <week-recommend :weekRecommend="weekRecommend"></week-recommend>
       home
       </div>
 </template>
@@ -10,9 +12,12 @@
 // 导入子组件
 const HomeNav =()=>import("./children/home-nav.vue")
 const myswiper = ()=>import("./children/myswiper.vue")
+const myrecommend = ()=>import("./children/myrecommend.vue")
+const weekRecommend = ()=>import("./children/week-recommend.vue")
 
 // 导入接口文件
-import {swiper,recommend} from "api/home.js"
+import {swiper,recommend,week} from "api/home.js"
+// import WeekRecommend from './children/week-recommend.vue'
 // import Myswiper from '../cart/children/myswiper.vue'
 export default {
       // 组件名称
@@ -22,18 +27,23 @@ export default {
       // 局部注册的组件
     components: {
       HomeNav,
-      myswiper
+      myswiper,
+      myrecommend,
+      weekRecommend
+        // WeekRecommend,
     },
       // 组件状态值
     data () {
         return {
           swiperImage : [],
           recommendData : [],
+          weekRecommend : [],
         }
     },
     created(){
       this.swiper();
       this.recommend();
+      this.getWeekRecommend();
     },
     methods : {
       // 请求数据方法
@@ -45,15 +55,21 @@ export default {
       },
       recommend(){
         recommend().then(res=>{
-          console.log(res);
-          this.recommendData = res.data;
+          console.log(res.data.recommend);
+          this.recommendData = res.data.recommend;
         })
+      },
+      getWeekRecommend(){
+        week().then(res=>{
+          console.log(res.data)
+          this.weekRecommend = res.data.week
+        });
       }
     }
 }
 </script>
 <style scoped>
 .home{
-  /* background-color:rgb(216, 212, 212); */
+  background-color:rgb(216, 212, 212);
 }
 </style>
