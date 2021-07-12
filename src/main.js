09e9ca78e985d3784a607c1b371d5aadac6ce62e
苟宏 <1878149080@qq.com>
2021-07-12 15:46:39 +0800

    将适配文件加入到工具文件中

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import 'amfe-flexible'
+// import 'amfe-flexible'
+import "utils/index.js"
 
 Vue.config.productionTip = false
 
